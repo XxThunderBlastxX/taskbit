d4870a675a5a0f76be59da4a06001dc1169ba880
XxThunderBlastxX <koustavmondal55@gmail.com>
2023-07-16 22:14:26 +0530

    Integrated completed tab

--- lib/src/feature/completed_tasks/presentation/completed_tab.dart
+++ lib/src/feature/completed_tasks/presentation/completed_tab.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../tasks/data/repository/task_repository.dart';
+import '../../tasks/domain/model/task_model/task_model.dart';
+import '../../tasks/presentation/provider/tasks_provider.dart';
+import '../../tasks/presentation/widgets/task_tile.dart';
+
 class CompletedTab extends ConsumerWidget {
   const CompletedTab({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Center(
-      child: Text('Completed'),
+    final taskList = ref.watch(taskNotifierProvider);
+
+    return ListView.builder(
+      itemCount: taskList.length,
+      itemBuilder: (context, index) {
+        return !taskList[index].isCompleted
+            ? const SizedBox()
+            : TaskTile(
+                title: taskList[index].title,
+                description: taskList[index].description,
+                category: taskList[index].category.name,
+                backgroundColor: taskList[index].category.taskCategoryColor,
+                onChanged: (value) =>
+                    ref.watch(taskRepositoryProvider).updateTask(
+                          docId: taskList[index].$id,
+                          task: TaskModel(
+                            userId: taskList[index].userId,
+                            title: taskList[index].title,
+                            description: taskList[index].description,
+                            isCompleted: value!,
+                            category: taskList[index].category,
+                            createdAt: taskList[index].createdAt,
+                            updatedAt: taskList[index].updatedAt,
+                          ),
+                        ),
+                value: taskList[index].isCompleted,
+              );
+      },
     );
   }
 }
